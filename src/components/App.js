9aaad0926af0392dcd6490cd9b89e8e1280ec3e4
tusharsharma000 <84301919+tusharsharma000@users.noreply.github.com>
2022-02-11 12:03:45 +0530

    String

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  );
+};
 
 export default App;
